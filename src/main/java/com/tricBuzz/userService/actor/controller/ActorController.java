f007f89cfb3f9e96b873cd527bebc296990c429b
kunalmanishdesai <kunalmanishdesai@yahoo.com>
2023-09-17 19:57:27 -0700

    updated apis

--- src/main/java/com/tricBuzz/userService/actor/controller/ActorController.java
+++ src/main/java/com/tricBuzz/userService/actor/controller/ActorController.java
@@ -1,12 +1,11 @@
 package com.tricBuzz.userService.actor.controller;
 
 import com.tricBuzz.userService.actor.model.ActorModel;
+import com.tricBuzz.userService.actor.model.ActorSearchModel;
 import com.tricBuzz.userService.actor.service.ActorService;
+import com.tricBuzz.userService.common.model.PaginationResponseModel;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,17 +15,16 @@ public class ActorController {
     private final ActorService actorService;
 
     @GetMapping("actors")
-    public List<ActorModel> get(@RequestParam("page") Integer page,
-                                @RequestParam("limit") Integer limit) {
-        return actorService.getActors(page,limit);
+    public PaginationResponseModel<ActorModel> search(@RequestBody ActorSearchModel actorSearchModel) {
+        return actorService.getActors(actorSearchModel);
     }
 
     @GetMapping("actors/{id}")
     public ActorModel get(@PathVariable Long id) {
-        return actorService.getActor(id);
+        return actorService.getActorModel(id);
     }
 
-    @PostMapping("actors/")
+    @PostMapping("actors")
     public ActorModel create(@RequestBody ActorModel model) {
         return actorService.createActor(model);
     }
