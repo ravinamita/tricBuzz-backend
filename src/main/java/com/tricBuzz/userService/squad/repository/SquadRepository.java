79e41130fbdbc867621eb0ed5939fae31553febc
kunalmanishdesai <kunalmanishdesai@yahoo.com>
2023-09-17 20:02:21 -0700

    Added Squad Repositories

---
+++ src/main/java/com/tricBuzz/userService/squad/repository/SquadRepository.java
@@ -0,0 +1,7 @@
+package com.tricBuzz.userService.squad.repository;
+
+import com.tricBuzz.userService.squad.entity.SquadEntity;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SquadRepository extends CrudRepository<SquadEntity, Long> {
+}
